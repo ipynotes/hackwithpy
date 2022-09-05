4d49001a701a2fa4962bc19a1a97472ebd0f50e8
KILLER.CODES <Killercodes@users.noreply.github.com>
2022-09-06 00:02:30 +0530

    Create scanner2.py

---
+++ buggysite/scanner2.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import socket
+import os
+import sys
+
+
+def retBanner(ip, port):
+    try:
+        socket.setdefaulttimeout(2)
+        s = socket.socket()
+        s.connect((ip, port))
+        banner = s.recv(1024)
+        return banner
+    except:
+        return
+
+
+def checkVulns(banner, filename):
+
+    f = open(filename, 'r')
+    for line in f.readlines():
+        if line.strip('\n') in banner:
+            print '[+] Server is vulnerable: ' +\
+                banner.strip('\n')
+
+
+def main():
+    print "vulnScanner is running..."
+    ipaddr = raw_input("Please enter IP Address as xxx.xxx.xxx = ")
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        if not os.path.isfile(filename):
+            print '[-] ' + filename +\
+                ' does not exist.'
+            exit(0)
+
+        if not os.access(filename, os.R_OK):
+            print '[-] ' + filename +\
+                ' access denied.'
+            exit(0)
+    else:
+        print '[-] Usage: ' + str(sys.argv[0]) +\
+            ' <vuln filename>'
+        exit(0)
+
+    portList = [21,22,25,80,110,443]
+    for x in range(1, 255):
+        ip = ipaddr + str(x)
+	print " - Scanning.. " + ip + "\r"
+        for port in portList:
+            banner = retBanner(ip, port)
+            if banner:
+                print '[+] ' + ip + ' : ' + banner
+                checkVulns(banner, filename)
+
+
+if __name__ == '__main__':
+    main()
